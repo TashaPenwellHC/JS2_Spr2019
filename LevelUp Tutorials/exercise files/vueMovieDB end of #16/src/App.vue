f52521660d23e33063d534d62258b204410383cf
TashaPenwellHC <38427291+TashaPenwellHC@users.noreply.github.com>
2019-04-08 12:48:27 -0400

    added exercise folders and files

---
+++ LevelUp Tutorials/exercise files/vueMovieDB end of #16/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" v-if="status === 'Ready'">
+    <Header :title="title" />
+    <MoviesList />
+  </div>
+  <div v-else-if="status === 'Loading'">
+    Loading
+  </div>
+  <div v-else>
+    Error
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import MoviesList from './components/MoviesList.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      hello: 'Hello World',
+      title: 'Vue Movie DB',
+      isTrue: true,
+      status: 'Ready'
+    };
+  },
+  components: {
+    MoviesList,
+    Header
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ LevelUp Tutorials/exercise files/vueMovieDB end of #9/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Header />
+    <img src="./assets/logo.png">
+    <h1>{{ hello }}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      hello: 'Hello World'
+    };
+  },
+  components: {
+    HelloWorld,
+    Header
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
